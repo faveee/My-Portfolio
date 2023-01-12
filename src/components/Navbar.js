bee70191c5636e4d9bb86d3dd8d2302216480ac3
faveee <abatangift11@gmail.com>
2023-01-12 21:52:51 +0100

    newly added

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Navbar() {
+    return(
+<main>
+    <section className="bg-red-300 min-h-screen">
+<nav className="flex justify-around">
+    <ul>
+        <li>
+            <a href="#">Home</a>
+        </li>
+        <li>
+            <a href="#">About</a>
+        </li>
+    </ul>
+</nav>
+    </section>
+</main>
+    )
+}
+
+export default Navbar
