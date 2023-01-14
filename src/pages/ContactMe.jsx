4e0ebef79e0f61f5618f7808748b0289d38069d9
faveee <abatangift11@gmail.com>
2023-01-14 09:28:10 +0100

    New Changes

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { AiOutlineTwitter, AiFillGithub, AiFillLinkedin} from "react-icons/ai";
+import { BiPhoneCall } from "react-icons/bi";
+import { ImLocation } from "react-icons/im";
 
 function ContactMe() {
     return(
@@ -7,13 +9,13 @@ function ContactMe() {
 <div>
     <h3>Get in touch</h3>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, nam inventore dignissimos voluptates voluptatibus corporis, rem, perspiciatis officia quasi unde itaque enim alias quibusdam nihil laboriosam voluptate maxime amet minima.</p>
-    <p>+2349030559945</p>
-    <p>Lagos, Nigeria</p>
+    <p><BiPhoneCall />+2349030559945</p>
+    <p><ImLocation />Lagos, Nigeria</p>
 </div>
 <div>
     <h2>Mail</h2>
     </div>
-    <div className="text-5xl flex justify-center gap-16">
+    <div className="text-5xl flex justify-center gap-16 text-gray-60">
 <AiOutlineTwitter />
 <AiFillGithub />
 <AiFillLinkedin />
