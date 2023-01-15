f0e89054562afea35d90fa944329593bd6099d16
faveee <abatangift11@gmail.com>
2023-01-15 11:24:00 +0100

    New changes

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -6,11 +6,11 @@ function ContactMe() {
         <section className="m-10">
 <div className="p-10">
     <h3 className="text-8xl font-extrabold text-red-500 pb-10 text-center">Get in touch</h3>
-    <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, nam inventore dignissimos voluptates voluptatibus corporis, rem, perspiciatis officia quasi unde itaque enim alias quibusdam nihil laboriosam voluptate maxime amet minima.</p>
+    <p className="">Have a frontend developer role you're looking to fill or a prpject with a need for my range of skill sets? I'm currently available to new opportunities. Kindly reach out!</p>
 </div>
 <div>
     <button className="bg-gradient-to-r from-cyan-500 to-blue-500
- text-white py-3 mb-5 text-center border-double hover:border-double border-4 rounded-lg">Let's talk</button>
+ text-white py-3 mb-5 text-center border-double hover:border-double border-4 rounded-lg"><a href="mailto:abatangift11@gmail.com">Let's talk</a></button>
  </div>
     <div className="text-5xl flex justify-center gap-16 text-gray-60 pt-10">
 <AiOutlineTwitter />
