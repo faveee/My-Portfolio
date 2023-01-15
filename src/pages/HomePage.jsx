44fdc1adcab7ddabad8eccb641c7d25bbd96630c
faveee <abatangift11@gmail.com>
2023-01-15 21:01:38 +0100

    New Commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,26 +1,28 @@
 import React from "react";
 import "./Homepage.css";
-import hero from "../../src/assets/hero.png"; 
+import Image from "../../src/assets/hero.jpg"; 
 
 function HomePage() {
-
     return(
         <body>
-<div className="m-11 p-20">
-    <h2 className="text-5xl text-left text-teal-800 font-extrabold m-0">Favour Abatan</h2>
+<div className="m-11 p-20 flex justify-between">
+    <h2 className="md:flex-row text-5xl text-left text-teal-800 font-extrabold m-0">Favour Abatan</h2>
     </div>
     <div className="hero">
         <ul className="dynamic-texts">
         <li><span>Developer.</span></li>
         <li><span>Freelancer.</span></li>
         </ul>
-        <img src={hero} alt="Hero"/>
 </div>
-<p className="pl-5 text-black py-5">I'm a software Developer, able to build a web presence from scratch.</p>
-<nav className="pt-5">
+<div className="flex flex-col md:flex-row justify-between gap-4">
+<p className="pl-5 text-black">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat fugit laborum cumque qui. Ex, ut voluptatem? Magni aspernatur beatae accusantium dolorum quidem qui eveniet inventore illum dignissimos. Voluptate non voluptatem culpa nesciunt, labore eligendi, consequatur dolor totam, officia maiores laborum deserunt. Voluptas culpa fugiat placeat sunt maiores? Nemo, aperiam ducimus.</p>
+<img className="rounded-full flex ml-auto" src={Image} alt="Hero"/>
+</div>
+<nav className="">
     <ul>
         <li>
-            <a  className="bg-gradient-to-r from-cyan-500 to-blue-500 text-white px-4 py-2 ml-10 my-5 border-double hover:border-double rounded-lg" href="https://drive.google.com/file/d/1YhOE-bDOXwyOriDFzqmabV7hjrmHv_Iz/view">
+            <a  className="bg-gradient-to-r from-cyan-500 to-blue-500
+ text-white py-3 mb-5 ml-5 px-8 text-center border-double hover:border-double border-4 rounded-lg" href="https://drive.google.com/file/d/1YhOE-bDOXwyOriDFzqmabV7hjrmHv_Iz/view">
                 Download Resume
             </a>
         </li>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import signup from "../../src/assets/signup.png"; 
 import todo from "../../src/assets/todo.png"; 
-import faq from "../../src/assets/faq.png"; 
+import sparzly from "../../src/assets/sparzly.png";
+import { AiFillGithub } from "react-icons/ai";
+import { FiExternalLink } from "react-icons/fi"
 
 function Projects() {
     return(
@@ -15,24 +17,56 @@ function Projects() {
         <h3 className="text-lg font-medium pt-8 pb-2">SignUp Page</h3>
         <p className="font-extralight pb-2">HTML, Css </p>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere vero rem mollitia ipsa earum iusto iure possimus minima debitis explicabo aliquam aperiam sed, enim optio. Quam provident quod tempora odio.</p>
+        <div className="flex gap-3 pt-5 text-2xl">
+        <a href="https://github.com/faveee/Sign-Up-Form.git" >
+<AiFillGithub />
+</a>
+<a href="https://faveee-signupform.netlify.app" >
+<FiExternalLink />
+</a>
+</div>
 </div>
 <div className="text-left shadow-lg p-11 rounded-xl my-[60px] mx-11">
         <img src={todo} alt="Todo-List" />
-        <h3 className="text-lg font-medium pt-8 pb-2">SignUp Page</h3>
+        <h3 className="text-lg font-medium pt-8 pb-2">Todo List</h3>
         <p className="font-extralight pb-2">HTML, Css, Javascript </p>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere vero rem mollitia ipsa earum iusto iure possimus minima debitis explicabo aliquam aperiam sed, enim optio. Quam provident quod tempora odio.</p>
+        <div className="flex gap-3 pt-5 text-2xl">
+        <a href="https://github.com/faveee/Todo-App.git" >
+<AiFillGithub />
+</a>
+<a href="https://favee-todoapp.netlify.app" >
+<FiExternalLink />
+</a>
 </div>
-<div className="text-left shadow-lg p-11 rounded-xl my-10 mx-10">
-        <img src={faq} alt="Signup"/>
-        <h3 className="text-lg font-medium pt-8 pb-2 mx-10">SignUp Page</h3>
-        <p className="font-extralight pb-2">HTML, Css </p>
+</div>
+<div className="text-left shadow-lg p-11 rounded-xl my-[60px] mx-11">
+        <img src={sparzly} alt="Sparzly"/>
+        <h3 className="text-lg font-medium pt-8 pb-2">Sparzly Food App</h3>
+        <p className="font-extralight pb-2">HTML, Css, Javascript </p>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere vero rem mollitia ipsa earum iusto iure possimus minima debitis explicabo aliquam aperiam sed, enim optio. Quam provident quod tempora odio.</p>
+        <div className="flex gap-3 pt-5 text-2xl">
+        <a href="https://github.com/faveee/Sparzly-Food.git" >
+<AiFillGithub />
+</a>
+<a href="#" >
+<FiExternalLink />
+</a>
 </div>
-<div className="text-left shadow-lg p-11 rounded-xl my-10 mx-10">
+</div>
+<div className="text-left shadow-lg p-11 rounded-xl my-[60px] mx-11">
         <img src={signup} alt="Signup"/>
         <h3 className="text-lg font-medium pt-8 pb-2">SignUp Page</h3>
         <p className="font-extralight pb-2">HTML, Css </p>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere vero rem mollitia ipsa earum iusto iure possimus minima debitis explicabo aliquam aperiam sed, enim optio. Quam provident quod tempora odio.</p>
+        <div className="flex gap-3 pt-5 text-2xl">
+        <a href="https://github.com/faveee" >
+<AiFillGithub />
+</a>
+<a href="#" >
+<FiExternalLink />
+</a>
+</div>
 </div>
 </div>
 </section>
